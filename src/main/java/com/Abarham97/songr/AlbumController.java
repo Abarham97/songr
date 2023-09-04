44b6db2cd97880c9cd651c81e7b67b299fe620e1
Abarham97 <abodzbarham@gmailcom>
2023-09-04 20:46:57 +0300

    changes

--- src/main/java/com/Abarham97/songr/AlbumController.java
+++ src/main/java/com/Abarham97/songr/AlbumController.java
@@ -2,21 +2,26 @@ package com.Abarham97.songr;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
+@Controller
 public class AlbumController {
     @GetMapping("/albums")
     public String listAlbums(Model model) {
 
         List<Album> albums = Arrays.asList(
-                new Album("Album1", "Artist1", 10, 300, "image1.jpg"),
-                new Album("Album2", "Artist2", 12, 360, "image2.jpg"),
-                new Album("Album3", "Artist3", 8, 240, "image3.jpg")
+              new Album("Album1", "Artist1", 10, 300, "/images/alubm1.jpg"),
+
+                new Album("Album2", "Artist2", 12, 360, "/images/albume2.jpg"),
+
+                new Album("Album3", "Artist3", 8, 240, "/images/album3.jpg")
         );
 
         model.addAttribute("albums", albums);
-        return "album-list";
+        return "album";
     }
+
 }
--- src/main/java/com/Abarham97/songr/SplashController.java
+++ src/main/java/com/Abarham97/songr/SplashController.java
@@ -1,7 +1,8 @@
 package com.Abarham97.songr;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+@Controller
 public class SplashController {
     @GetMapping("/")
     public String splash() {
