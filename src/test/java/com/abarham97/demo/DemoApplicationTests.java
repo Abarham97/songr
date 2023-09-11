94d8ad25a1351388bb34dd7b3d3fe8e56ef33f46
Abarham97 <abodzbarham@gmailcom>
2023-09-11 20:49:25 +0300

    lab13-2

--- src/test/java/com/abarham97/demo/DemoApplicationTests.java
+++ src/test/java/com/abarham97/demo/DemoApplicationTests.java
@@ -2,12 +2,28 @@ package com.abarham97.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+	public class HelloWorldControllerIntegrationTest {
+
+		@LocalServerPort
+		private int port;
+
+		private final TestRestTemplate restTemplate = new TestRestTemplate();
+
+		@Test
+		public void testHelloEndpoint() {
+			String url = "http://localhost:" + port + "/hello";
+			String response = restTemplate.getForObject(url, String.class);
+
+			assertThat(response).contains("Hello, World!");
+		}
 	}
 
 }
